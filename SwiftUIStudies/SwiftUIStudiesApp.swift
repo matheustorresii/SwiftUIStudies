3bc43fcbff1407286564bbf1c674d25f97a38106
matheustorres <matheustf2003@gmail.com>
2021-02-03 15:20:43 -0300

    Initial Commit

---
+++ SwiftUIStudies/SwiftUIStudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudiesApp.swift
+//  SwiftUIStudies
+//
+//  Created by Matheus Torres on 03/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
